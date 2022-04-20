557dec0c66a9b2886ef542ee3287e8bdf4df4a7a
Robert Aguilar <robeagui@gmail.com>
2022-04-20 17:09:09 -0400

    fastq-dump yes no

--- fastq_dump_tools.py
+++ fastq_dump_tools.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import shutil
 
 #you need to install pytul using your terminal "pip3 install python-util"
 from pyutil import filereplace
@@ -16,11 +17,12 @@ filereplace('commands_srr.txt', 'truseq3', tru_seq)
 bowtie = input('Copy and paste the complete path to your bowtie files: ')
 filereplace('commands_srr.txt', 'bowtie2_path', bowtie)
 
+#add the path to where reference chromosome files are found
 ref_chrom = input('Copy and paste the complete path to your reference chromosome: ')
-filereplace('commands_srr.txt', '/mnt/d/sra/brca/bgz/Homo_sapiens.GRCh38.dna.chromosome.10', ref_chrom)
+filereplace('commands_srr.txt', 'ref_chrom', ref_chrom)
 
 #this asks user to type in 10 accession numbers
-'''srr_one = input('Please paste the 1st SRA accession number: ')
+srr_one = input('Please paste the 1st SRA accession number: ')
 srr_two = input('Please paste the 2nd SRA accession number: ')
 srr_thr = input('Please paste the 3rd SRA accession number: ')
 srr_fou = input('Please paste the 4th SRA accession number: ')
@@ -47,4 +49,37 @@ filereplace('commands_srr.txt',"SRR_nin", srr_nin)
 filereplace('commands_srr.txt',"SRR_ten", srr_ten)
 
 #this will run the commands on the commands_srr.txt file
-os.system('cat commands_srr.txt | bash')'''
+os.system('cat commands_srr.txt | bash')
+
+print('*** The previous input sequences have been analyzed.')
+
+# getting the current working directory
+src_dir = os.getcwd()
+
+# printing current directory
+print('*** This is your current working directory:' + src_dir)
+
+# printing the list of new files
+print('*** These are the files in the present directory: ') 
+print(os.listdir())
+
+# copying the files
+shutil.copyfile('commands_srr_template_gen.txt', 'commands_srr.txt') #copy src to dst
+
+print('*** fastq_dump_tools is ready to run.')
+
+print('*** Would you like to run another analysis? ')
+
+while True:
+    a = input('Enter yes/no to continue: ')
+    if a=="yes":
+        os.system('python3 fastq_dump_tools.py')
+        continue
+    elif a=="no":
+        print('Analysis terminated.')
+        break
+    else:
+        print('Enter either yes/no: ')
+
+
+
