a60fccc33c3f7ee4840cc33995d7d62e29d4ee2c
Robert Aguilar <robeagui@gmail.com>
2022-04-20 15:14:50 -0400

    Paths
    User input in order to modify paths to specific files.

--- fastq_dump_tools.py
+++ fastq_dump_tools.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
-
 import os
+
+#you need to install pytul using your terminal "pip3 install python-util"
+from pyutil import filereplace
+
+#add the path to where trimmomatic-0.39.jar is found
+trim = input('Copy and paste the complete path to your trimmomatic-0.39.jar file: ')
+filereplace('commands_srr.txt',"trim_path", trim)
+
+#add the path to where TruSeq3 file is found
+tru_seq = input('Copy and paste the complete path to your TruSeq3 file: ')
+filereplace('commands_srr.txt', 'truseq3', tru_seq)
+
+#add the path to where bowtie files are found (must end in "bowtie/bowtie")
+bowtie = input('Copy and paste the complete path to your bowtie files: ')
+filereplace('commands_srr.txt', 'bowtie2_path', bowtie)
+
+ref_chrom = input('Copy and paste the complete path to your reference chromosome: ')
+filereplace('commands_srr.txt', '/mnt/d/sra/brca/bgz/Homo_sapiens.GRCh38.dna.chromosome.10', ref_chrom)
+
 #this asks user to type in 10 accession numbers
-srr_one = input('Please paste the 1st SRA accession number: ')
+'''srr_one = input('Please paste the 1st SRA accession number: ')
 srr_two = input('Please paste the 2nd SRA accession number: ')
 srr_thr = input('Please paste the 3rd SRA accession number: ')
 srr_fou = input('Please paste the 4th SRA accession number: ')
@@ -13,8 +31,7 @@ srr_eig = input('Please paste the 8th SRA accession number: ')
 srr_nin = input('Please paste the 9th SRA accession number: ')
 srr_ten = input('Please paste the 10th SRA accession number: ')
 
-#you need to install pytul using your terminal "pip3 install python-util"
-from pyutil import filereplace
+
 
 #these commands will replace each SRR number on .txt file with 
 #each of the ten accession numbers entered by user
@@ -30,4 +47,4 @@ filereplace('commands_srr.txt',"SRR_nin", srr_nin)
 filereplace('commands_srr.txt',"SRR_ten", srr_ten)
 
 #this will run the commands on the commands_srr.txt file
-os.system('cat commands_srr.txt | bash')
+os.system('cat commands_srr.txt | bash')'''
