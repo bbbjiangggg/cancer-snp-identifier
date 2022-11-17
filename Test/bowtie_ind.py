f74e2b326594fe5e6cfd3f3831f199712c6c4557
Robert Aguilar <robeagui@gmail.com>
2022-11-17 16:07:47 -0500

    index

---
+++ Test/bowtie_ind.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+
+#bowtie is used to index the reference chromosome
+
+def start():
+    #make necessary directory
+    number = input('\033[1;45m Please enter a valid chromosome number: \033[0;0;0m')
+    print('\n')
+
+    isvalid = number.startswith('0')
+    greater = int(number) < 23
+
+    while ((isvalid == False) and (greater == True)):
+        os.system('mkdir bowtie_ind_'+ number)
+        os.system('cd bowtie_ind_'+ number)
+        
+        print('\033[1;45m Downloading Chromosome '+ number + '\033[0;0;0m' )
+        os.system('wget http://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.'+ number +'.fa.gz')
+
+        #download chromosome sequence
+        os.system('mv *.gz* bowtie_ind_'+ number)
+
+        #indexing chromosome file
+        print('\033[1;45m Indexing with Bowtie... \033[0;0;0m' )
+        os.chdir('bowtie_ind_' + number)
+        os.system('bowtie2-build *.gz* bowtie')
+        
+        print('\033[1;45m Indexing with Bowtie complete \033[0;0;0m' )
+        print('\n')
+        print('\033[1;45m Your path to the bowtie index of chromsome ' + number + ' is: \033[0;0;0m' )
+        os.system('realpath bowtie')
+        exit()
+    else:
+        print('\033[0;101m NOTE: Do not include a "0" in front of the number. \033[0;0;0m')
+        print('\033[0;101m The number must be less than 23. \033[0;0;0m')
+        
+        print('\n')
+        start()
+     
+
+start()
+    
+
+            
