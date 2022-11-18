e4bcfa050c5485b5ff197bbeb42e3b9941b96e86
Robert Aguilar <robeagui@gmail.com>
2022-11-18 17:44:46 -0500

    mac

--- installation/install_mac_prog02.py
+++ installation/install_mac_prog02.py
@@ -8,10 +8,71 @@ print("\033[1;45m ONLY RUN THIS PROGRAM IN YOUR HOME DIRECTORY  \033[0;0;0m")
 print("\033[1;45m to access your home directory open Finder and then click on Go and Home.  \033[0;0;0m")
 input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
 
-print("\033[1;45m Installing Python3 and pip3 \033[0;0;0m")
-subprocess.call(['sudo', 'apt', 'install', 'python3', 'python3-pip'])
+#checking for the installation of xcode
+if os.path.exists("/Applications/Xcode.app"):
+    print("Xcode is installed")
+else:
+    print("Xcode is not installed")
+    print("Please install Xcode from the App Store")
+    sys.exit()
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+#checking for installation of homebrew
+print("\033[1;45m Checking for installation of homebrew \033[0;0;0m")
+if os.system("brew --version") == 0:
+    print("\033[1;45m homebrew is installed \033[0;0;0m")
+else:
+    print("\033[1;45m homebrew is not installed \033[0;0;0m")
+    print("\033[1;45m installing homebrew \033[0;0;0m")
+    os.system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)\"")
+    print("\033[1;45m homebrew is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+#checking for installation of python3
+print("\033[1;45m Checking for installation of python3 \033[0;0;0m")
+if os.system("python3 --version") == 0:
+    print("\033[1;45m python3 is installed \033[0;0;0m")
+else:
+    print("\033[1;45m python3 is not installed \033[0;0;0m")
+    print("\033[1;45m installing python3 \033[0;0;0m")
+    os.system("brew install python3")
+    print("\033[1;45m python3 is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+#checking for installation of pip3
+print("\033[1;45m Checking for installation of pip3 \033[0;0;0m")
+if os.system("pip3 --version") == 0:
+    print("\033[1;45m pip3 is installed \033[0;0;0m")
+else:
+    print("\033[1;45m pip3 is not installed \033[0;0;0m")
+    print("\033[1;45m installing pip3 \033[0;0;0m")
+    os.system("brew install pip3")
+    print("\033[1;45m pip3 is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
 
 import pip 
+
+#checking for the installation of wget
+print("\033[1;45m Checking for installation of wget \033[0;0;0m")
+if os.system("wget --version") == 0:
+    print("\033[1;45m wget is installed \033[0;0;0m")
+else:
+    print("\033[1;45m wget is not installed \033[0;0;0m")
+    print("\033[1;45m installing wget \033[0;0;0m")
+    os.system("brew install wget")
+    print("\033[1;45m wget is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
 #Install necessaries 
 print("\033[1;45m Install Homebrew, vim, and wget \033[0;0;0m")
 input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
