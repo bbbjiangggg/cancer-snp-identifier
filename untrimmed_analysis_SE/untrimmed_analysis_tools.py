af8f0513f9c94874e3f9a727bb66abfddaaba340
Robert Aguilar <robeagui@gmail.com>
2022-11-21 21:26:08 -0500

    untrim

--- untrimmed_analysis_SE/untrimmed_analysis_tools.py
+++ untrimmed_analysis_SE/untrimmed_analysis_tools.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 
+
 #THIS PROGRAM IS FOR FULL ANALYSIS
 
 #you need to install pytul using your terminal "pip3 install python-util"
@@ -26,19 +27,23 @@ filereplace('sendemail.txt', 'job_name', job)
 
 #add the path to where trimmomatic-0.39.jar is found
 trim = input('Copy and paste the complete path to your trimmomatic-0.39.jar file: ')
-filereplace('commands_srr.txt',"trim_path", trim)
+filereplace('untrimmed_bash_srr.txt',"trim_path", trim)
 
 #add the path to where TruSeq3 file is found
 tru_seq = input('Copy and paste the complete path to your TruSeq3 file: ')
-filereplace('commands_srr.txt', 'truseq3_path', tru_seq)
+filereplace('untrimmed_bash_srr.txt', 'truseq3_path', tru_seq)
 
 #add the path to where bowtie files are found (must end in "bowtie")
 bowtie = input('Copy and paste the complete path to your Bowtie files: ')
-filereplace('commands_srr.txt', 'bowtie2_path', bowtie)
+filereplace('untrimmed_bash_srr.txt', 'bowtie2_path', bowtie)
 
 #add the path to where reference chromosome is found
 ref_chrom = input('Copy and paste the complete path to your BWA reference chromosome: ')
-filereplace('commands_srr.txt', 'ref_chrom', ref_chrom)
+filereplace('untrimmed_bash_srr.txt', 'ref_chrom', ref_chrom)
+
+#make a copy of commands_srr
+os.system('cp untrimmed_bash_srr.txt copy_untrimmed_bash_srr.txt')
+
 
 #this asks user to type in the path to the accession list
 accession = input('Copy and paste the name of the accesion list file (make sure it is in the same directory): ')
@@ -49,6 +54,9 @@ with open(accession,'r') as file:
     print(f'There are {len(line)} unanalyzed sequences')
     file.close()
 
+#this creates a copy of the accession list
+os.system('cp ' + accession + ' copy_' + accession)
+    
 #this asks user to type in accession numbers
 number = int(input('How many SRA sequences do you wish to analyze: '))
 
@@ -60,35 +68,35 @@ placement = [ordinal(n) for n in range(1, number)]
 #this will set different variables for different srr sequences
 srr_list = line[0:number-1]
 
-
+ 
 #these commands will replace each SRR number on .txt file with 
 #each of the accession numbers entered by user
 for index in range(len(srr_list)):
-    filereplace('commands_srr.txt',"number", placement[index])
-    filereplace('commands_srr.txt',"SRR_one", srr_list[index])
+    filereplace('untrimmed_bash_srr.txt',"number", placement[index])
+    filereplace('untrimmed_bash_srr.txt',"SRR_one", srr_list[index])
 
     #run the commands on the commands_srr2.txt file
-    os.system('cat commands_srr.txt | bash')
+    os.system('cat untrimmed_bash_srr.txt | bash')
 
     #replace the changed names back to orginal
-    filereplace('commands_srr.txt', placement[index], "number")
-    filereplace('commands_srr.txt', srr_list[index], "SRR_one")
+    filereplace('untrimmed_bash_srr.txt', placement[index], "number")
+    filereplace('untrimmed_bash_srr.txt', srr_list[index], "SRR_one")
 
 #print the command has been down
 print('\033[1;45m The previous input sequences have been analyzed.\033[0m')
 
 #run the commands on the sendemail.txt file
-#os.system('cat sendemail.txt | bash')
+os.system('cat sendemail.txt | bash')
 
 #reset the sendemail command
 filereplace('sendemail.txt', user, 'user_email')
 filereplace('sendemail.txt', job, 'job_name')
 
 #reset the bowtie2_path, refchrome, trimpath, and truseq path
-filereplace('commands_srr.txt', bowtie, 'bowtie2_path')
-filereplace('commands_srr.txt', ref_chrom, 'ref_chrom')
-filereplace('commands_srr.txt', trim, "trim_path")
-filereplace('commands_srr.txt', tru_seq, 'truseq3_path')
+filereplace('untrimmed_bash_srr.txt', bowtie, 'bowtie2_path')
+filereplace('untrimmed_bash_srr.txt', ref_chrom, 'ref_chrom')
+filereplace('untrimmed_bash_srr.txt', trim, "trim_path")
+filereplace('untrimmed_bash_srr.txt', tru_seq, 'truseq3_path')
 
 #this will remove the analyzed sequences from the accession list
 with open(accession,'w') as file:
@@ -96,17 +104,21 @@ with open(accession,'w') as file:
         if content not in srr_list:
             file.write(content + '\n')
 
+os.system('rm copy_untrimmed_bash_srr.txt')
 
-print('\033[1;45m fastq_dump_tools is ready to run.\033[0;0;0m')
+print('\033[1;45m untrimmed_analysis_tools is ready to run.\033[0;0;0m')
 
 print('\033[1;45m Would you like to run another analysis? \033[0;0;0m')
 
 while True:
     a = input('Enter yes/no to continue: ')
     if a=="yes":
+        print('\033[1;45m This was the email address used:\033[0;0;0m ', user)
+        print('\033[1;45m This was your trim.jar file path:\033[0;0;0m ', trim)
+        print('\033[1;45m This was your TruSeq3 file path:\033[0;0;0m ', tru_seq)
         print('\033[1;45m This was your Bowtie files path:\033[0;0;0m ', bowtie)
         print('\033[1;45m This was your BWA reference chromosome path:\033[0;0;0m ', ref_chrom)
-        os.system('python3 wholeAnalysis.py')
+        os.system('python3 untrimmed_analysis_tools.py')
         continue
     elif a=="no":
         print('\033[1;45m Analysis terminated. Goodbye. \033[0;0;0m')
