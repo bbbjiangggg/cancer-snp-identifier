32f120407b78c9d0918344685b4f32548f297a29
Robert Aguilar <robeagui@gmail.com>
2022-12-02 14:37:49 -0500

    count

--- cell_count.py
+++ cell_count.py
@@ -1,22 +1,31 @@
+#!/usr/bin/env python3
+
+
+plate = input('\033[1;45m Enter the plate you wish to use (96-wells, 24-wells, 6-wells): \033[0;0;0m')
+
 #number of wells
-wells = input('\033[1;45m Enter the number of wells you wish to use: \033[0;0;0m ')
+wells = input('\033[1;45m Enter the number of wells from the ' + plate + ' you wish to use: \033[0;0;0m ')
 print('\n')
 
-inwellvol = input('\033[1;45m Enter the intended volume (ml) for each well: \033[0;0;0m ') 
+inwellvol = input('\033[1;45m Enter the intended volume (ml) for each well of the ' + plate + ' (number only): \033[0;0;0m ')
+if inwellvol == int:
+    inwellvol = float(inwellvol)
+else:
+    print('\033[1;31m Please enter a number only \033[0;0;0m') 
 print('\n')
 
 #well wellvol calculation
 
 wellvol = float(inwellvol) * float(wells) + 1
 
-print('\033[1;45m You will now enter the total number of counted live cells (CLC) in the sample. \033[0;0;0m ')
+print('\033[1;45m You will now enter the total number of live cell count in the sample. \033[0;0;0m ')
 print('\033[1;45m You will enter the number using the following format: \033[0;0;0m 7.67e6  ')
 print('\033[1;45m This means 7.67 million cells \033[0;0;0m ')
 input('\033[1;45m Press enter to continue \033[0;0;0m ')
 print('\n')
 
 #number of live cells in the sample
-cells = float(input('\033[1;45m Enter the number of CLC: \033[0;0;0m '))
+cells = float(input('\033[1;45m Enter the number of live cell count: \033[0;0;0m '))
 print('\n')
 density = float(input('\033[1;45m Enter the seeding density you wish to use in your experiment (e.g. 1.0e5): \033[0;0;0m '))
 print('\n')
@@ -36,7 +45,7 @@ else:
 print('\n')
 
 print('\033[1;45m Therefore, mix: \033[0;0;0m ')
-print(str(micro) + ' \u03BCl of CLC \033[0;0;0m ')
+print(str(micro) + ' \u03BCl of the live cell count \033[0;0;0m ')
 print('with ' + str(new) + ' ml of new medium ')
 print('to get a seeding density of ' + str(density) + ' cells/ml')
 
