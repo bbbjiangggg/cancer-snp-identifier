6b3d1c31c81a3458a764f68aeebaabb7bbd86c8d
Robert Aguilar <robeagui@gmail.com>
2023-03-09 22:49:47 -0500

    install

---
+++ installation/install_linux_prog_v1.1.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+# Define color codes
+RED = '\033[1;31m'
+GREEN = '\033[1;32m'
+MAGENTA = '\033[1;35m'
+BLUE = '\033[1;34m'
+RESET = '\033[0m'
+
+def check_install(package):
+    print(f"{MAGENTA}\nChecking for the installation of {package}{RESET}")
+
+    if subprocess.call(['which', package]) == 0:
+        print(f"{GREEN}{package} is installed{RESET}")
+    else:
+        print(f"{RED}{package} is not installed{RESET}")
+        print(f"{MAGENTA}installing {package}{RESET}")
+        subprocess.check_call(['sudo', 'apt', 'install', package, '-y'])
+        print(f"{GREEN}{package} is installed{RESET}")
+
+print(f"{RED}\nONLY RUN THIS PROGRAM IN YOUR HOME DIRECTORY{RESET}")
+print(f"{MAGENTA}\nTo access your home directory use the command: '$ explorer.exe .' only on Ubuntu WSL{RESET}")
+
+input(f"{BLUE}\nPress enter to continue...{RESET}\n")
+
+print(f"{MAGENTA}\nInstalling Python3 and pip3{RESET}")
+subprocess.check_call(['sudo', 'apt-get', '-y', 'install', 'python3', 'python3-pip'])
+
+print(f"{MAGENTA}\nInstalling pyutil and pandas{RESET}")
+subprocess.check_call(['pip3', 'install', 'pyutil', 'pandas'])
+
+print(f"{MAGENTA}\nInstalling biopython{RESET}")
+subprocess.check_call(['pip3', 'install', 'biopython'])
+
+############################################################################
+
+# Update terminal and install necessaries
+print(f"{MAGENTA}\nUpdating terminal{RESET}")
+input(f"{BLUE}\nPress enter to continue...{RESET}\n")
+
+#subprocess.check_call(['sudo', 'apt', 'update', '-y'])
+#subprocess.check_call(['sudo', 'apt', 'upgrade', '-y'])
+print('\n')
+
+# Checking for the installation of wget
+check_install('wget')
+input(f"{BLUE}\nPress enter to continue...{RESET}\n")
+print('\n')
+
+# Check and install unzip
+check_install('unzip')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+# Check and install sendemail
+check_install('sendemail')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+# Check and install tabix
+check_install('tabix')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+########################################################################
+
+print(f'{BLUE}Downloading SRA Toolkit{RESET}')
+subprocess.run(['wget', 'https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz'])
+subprocess.run(['tar', '-xvzf', 'sratoolkit.current-ubuntu64.tar.gz'])
+subprocess.run(['rm', 'sratoolkit.current-ubuntu64.tar.gz'])
+subprocess.run(['mv', 'sratoolkit.2.10.9-ubuntu64', 'sratoolkit'])
+subprocess.run(['export', 'PATH=$PATH:~/sratoolkit/bin'], shell=True)
+print(f'{GREEN}SRA Toolkit is downloaded{RESET}')
+
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+print('\n')
+
+# Install FASTQC
+print(f'{MAGENTA}Installing FASTQC{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+subprocess.run(['sudo', 'apt-get', 'install', '-y', 'fastqc'])
+print(f'{GREEN}FASTQC is installed{RESET}\n')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+##############################################################################
+
+# checking for the installation of java
+print(f'{MAGENTA}Checking for the installation of java{RESET}')
+if subprocess.run(['java', '-version']).returncode == 0:
+    print(f'{GREEN}java is installed{RESET}')
+else:
+    print(f'{RED}java is not installed{RESET}')
+    print(f'{MAGENTA}installing java{RESET}')
+
+
+# check if default-jre is installed
+print(f'{MAGENTA}Checking default-jre{RESET}')
+result = subprocess.run(['java', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if 'command not found' in result.stderr.decode('utf-8'):
+    print(f'{RED}default-jre is not installed{RESET}')
+    # install default-jre
+    print(f'{MAGENTA}Installing default-jre{RESET}')
+    subprocess.run(['sudo', 'apt-get', 'install', 'default-jre', '-y'])
+    print(f'{GREEN}default-jre is installed{RESET}')
+else:
+    print(f'{GREEN}default-jre is already installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+print()
+
+# check if default-jdk is installed
+print(f'{MAGENTA}Checking default-jdk{RESET}')
+result = subprocess.run(['javac', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if 'command not found' in result.stderr.decode('utf-8'):
+    print(f'{RED}default-jdk is not installed{RESET}')
+    # install default-jdk
+    print(f'{MAGENTA}Installing default-jdk{RESET}')
+    subprocess.run(['sudo', 'apt-get', 'install', 'default-jdk'])
+    print(f'{GREEN}default-jdk is installed{RESET}')
+else:
+    print(f'{GREEN}default-jdk is already installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+print()
+
+
+# create directory $HOME/local/bin if it does not exist
+subprocess.run(['mkdir', '-p', '$HOME/local/bin'])
+
+# install Trimmomatic
+print(f'{MAGENTA}Installing Trimmomatic{RESET}')
+subprocess.run(['wget', 'http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip'])
+subprocess.run(['unzip', 'Trimmomatic-0.39.zip'])
+subprocess.run(['cp', 'Trimmomatic-0.39/trimmomatic-0.39.jar', '$HOME/local/bin'])
+subprocess.run(['chmod', 'a+x', '$HOME/local/bin/trimmomatic-0.39.jar'])
+print(f'{GREEN}Trimmomatic is installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+print()
+
+##############################################################################
+
+# check if bwa is installed
+result = subprocess.run(['which', 'bwa'], capture_output=True, text=True)
+if not result.stdout.strip():
+    print(f'{MAGENTA}Installing BWA{RESET}')
+    subprocess.run(['sudo', 'apt-get', 'install', 'bwa', '-y'])
+else:
+    print(f'{GREEN}BWA is already installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+# check if bowtie2 is installed
+result = subprocess.run(['which', 'bowtie2'], capture_output=True, text=True)
+if not result.stdout.strip():
+    print(f'{MAGENTA}Installing Bowtie2{RESET}')
+    subprocess.run(['sudo', 'apt-get', 'install', 'bowtie2', '-y'])
+else:
+    print(f'{GREEN}Bowtie2 is already installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+# check if samtools is installed
+result = subprocess.run(['which', 'samtools'], capture_output=True, text=True)
+if not result.stdout.strip():
+    print(f'{MAGENTA}Installing samtools{RESET}')
+    subprocess.run(['sudo', 'apt-get', 'install', 'samtools', '-y'])
+else:
+    print(f'{GREEN}samtools is already installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+# check if bcftools is installed
+result = subprocess.run(['which', 'bcftools'], capture_output=True, text=True)
+if not result.stdout.strip():
+    print(f'{MAGENTA}Installing bcftools{RESET}')
+    subprocess.run(['sudo', 'apt-get', 'install', 'bcftools', '-y'])
+else:
+    print(f'{GREEN}bcftools is already installed{RESET}')
+input(f'{BLUE}Press enter to continue...{RESET}\n')
+
+print(f'{GREEN}All tools have been installed.{RESET}')
+print(f'{MAGENTA}Use the command "vdb-config -i" to configure your SRA Toolkit.{RESET}')
---
+++ test/install_mac_prog_v1.1.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+# Define color codes
+RED = '\033[1;31m'
+GREEN = '\033[1;32m'
+MAGENTA = '\033[1;35m'
+BLUE = '\033[1;34m'
+RESET = '\033[0m'
+
+print(f'{MAGENTA}ONLY RUN THIS PROGRAM IN YOUR HOME DIRECTORY{RESET}')
+print(f'{MAGENTA}To access your home directory, open Finder and then click on Go and Home.{RESET}')
+input(f'{MAGENTA}Press enter to continue...{RESET}\n')
+
+# Checking for the installation of Xcode
+print(f'{MAGENTA}Checking for the installation of Xcode{RESET}')
+result = subprocess.run(['xcode-select', '-p'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if 'error' in result.stderr.decode('utf-8'):
+    print(f'{RED}Xcode is not installed{RESET}')
+    # Install Xcode command line tools
+    print(f'{MAGENTA}Installing Xcode command line tools{RESET}')
+    subprocess.run(['xcode-select', '--install'])
+    input(f'{MAGENTA}Press enter to continue...{RESET}\n')
+else:
+    print(f'{GREEN}Xcode is installed{RESET}')
+    input(f'{MAGENTA}Press enter to continue...{RESET}\n')
+
+# Checking for installation of Homebrew
+print(f'{MAGENTA}Checking for installation of Homebrew{RESET}')
+result = subprocess.run(['brew', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if 'command not found' in result.stderr.decode('utf-8'):
+    print(f'{RED}Homebrew is not installed{RESET}')
+    # Install Homebrew
+    print(f'{MAGENTA}Installing Homebrew{RESET}')
+    subprocess.run(['/bin/bash', '-c', '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)'])
+    print(f'{GREEN}Homebrew is installed{RESET}')
+else:
+    print(f'{GREEN}Homebrew is installed{RESET}')
+
+input(f'{MAGENTA}Press enter to continue...{RESET}\n')
+
+#1 ###################################################################################
+
+
+#checking for installation of python3
+print("\033[1;45m Checking for installation of Python3 \033[0;0;0m")
+if os.system("python3 --version") == 0:
+    print("\033[1;45m Python3 is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Python3 is not installed \033[0;0;0m")
+    print("\033[1;45m installing Python3 \033[0;0;0m")
+    os.system("brew install python3")
+    print("\033[1;45m Python3 is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+#checking for installation of pip3
+print("\033[1;45m Checking for installation of Pip3 \033[0;0;0m")
+if os.system("pip3 --version") == 0:
+    print("\033[1;45m Pip3 is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Pip3 is not installed \033[0;0;0m")
+    print("\033[1;45m installing Pip3 \033[0;0;0m")
+    os.system("brew install pip3")
+    print("\033[1;45m Pip3 is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+import pip 
+
+os.system('pip3 install python-util')
+os.system('pip3 install pandas')
+subprocess.run(['pip', 'install', 'biopython'])
+
+
+#checking for the installation of wget
+print("\033[1;45m Checking for installation of Wget \033[0;0;0m")
+if os.system("wget --version") == 0:
+    print("\033[1;45m Wget is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Wget is not installed \033[0;0;0m")
+    print("\033[1;45m installing Wget \033[0;0;0m")
+    os.system("brew install wget")
+    print("\033[1;45m Wget is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+# checking for the installation of sendemail
+print("\033[1;45m Checking for installation of Sendemail \033[0;0;0m")
+if os.system("sendemail --version") == 0:
+    print("\033[1;45m Sendemail is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Sendemail is not installed \033[0;0;0m")
+    print("\033[1;45m installing Sendemail \033[0;0;0m")
+    os.system("brew install sendemail")
+    print("\033[1;45m Sendemail is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+# checking for the installation of tabix
+print("\033[1;45m Checking for installation of Tabix \033[0;0;0m")
+if os.system("tabix --version") == 0:
+    print("\033[1;45m Tabix is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Tabix is not installed \033[0;0;0m")
+    print("\033[1;45m installing Tabix \033[0;0;0m")
+    os.system("brew install Tabix")
+    print("\033[1;45m Tabix is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+# checking for the installation of gzip
+print("\033[1;45m Checking for installation of Gzip \033[0;0;0m")
+if os.system("gzip --version") == 0:
+    print("\033[1;45m Gzip is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Gzip is not installed \033[0;0;0m")
+    print("\033[1;45m installing Gzip \033[0;0;0m")
+    os.system("brew install gzip")
+    print("\033[1;45m Gzip is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+# checking for the installation of coreutils
+print("\033[1;45m Checking for installation of Coreutils \033[0;0;0m")
+if os.system("gsha256sum --version") == 0:
+    print("\033[1;45m Coreutils is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Coreutils is not installed \033[0;0;0m")
+    print("\033[1;45m installing Coreutils \033[0;0;0m")
+    os.system("brew install coreutils")
+    print("\033[1;45m Coreutils is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+#downloading SRA Toolkit
+print("\033[1;45m Downloading SRA Toolkit \033[0;0;0m")
+os.system('curl -OL --output sratoolkit.tar.gz http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-mac64.tar.gz')
+print("\033[1;45m SRA Toolkit downloaded \033[0;0;0m")
+print('\n')
+
+#extract SRA toolkit tar file contents
+print("\033[1;45m Extracting SRA Toolkit \033[0;0;0m")
+os.system('gunzip sratoolkit.current-mac64.tar.gz')
+os.system('tar -vxzf sratoolkit.current-mac64.tar')
+print("\033[1;45m SRA Toolkit extracted \033[0;0;0m")
+
+#remove SRA Toolkit tar file
+os.system('rm sratoolkit.current-mac64.tar')
+os.system('export PATH=$PATH:~/sratoolkit/bin')
+
+#install fastqc
+print("\033[1;45m Installing FastQC \033[0;0;0m")
+os.system("brew install fastqc")
+
+print("\033[1;45m FastQC installed \033[0;0;0m")
+print('\n')
+
+
+#checking for the installation of java
+print("\033[1;45m Checking for installation of Java \033[0;0;0m")
+if os.system("java -version") == 0:
+    print("\033[1;45m Java is installed \033[0;0;0m")
+else:
+    print("\033[1;45m Java is not installed \033[0;0;0m")
+    print("\033[1;45m installing Java \033[0;0;0m")
+    os.system("brew cask install java")
+    os.system("brew install openjdk")
+    print("\033[1;45m Java is installed \033[0;0;0m")
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+#install trimmomatic
+print("\033[1;45m Installing Trimmomatic \033[0;0;0m")
+os.system('wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip')
+os.system('unzip Trimmomatic-0.39.zip')
+os.system('rm Trimmomatic-0.39.zip')
+os.system('mkdir -p local/bin')
+os.system('cp Trimmomatic-0.39/trimmomatic-0.39.jar $HOME/local/bin')
+print("\033[1;45m Trimmomatic installed \033[0;0;0m")
+input('\033[1;45m Press enter to test Trimmomatic... \033[0;0;0m \n')
+print('\033[1;45m Checking if Trimmomatic was properly installed \033[0;0;0m')
+os.system('java -jar $HOME/local/bin/trimmomatic-0.39.jar')
+
+input('\033[1;45m Press enter to continue... \033[0;0;0m \n')
+print('\n')
+
+
+#install bwa
+print("\033[1;45m Installing BWA \033[0;0;0m")
+os.system("brew install bwa")
+print("\033[1;45m BWA installed \033[0;0;0m")
+print('\n')
+
+#install bowtie2
+print("\033[1;45m Installing Bowtie2 \033[0;0;0m")
+os.system("brew install bowtie2")
+print("\033[1;45m Bowtie2 installed \033[0;0;0m")
+print('\n')
+
+#install samtools
+print("\033[1;45m Installing SAMtools \033[0;0;0m")
+os.system("brew install samtools")
+print("\033[1;45m SAMtools installed \033[0;0;0m")
+print('\n')
+
+#install bcftools
+print("\033[1;45m Installing BCFtools \033[0;0;0m")
+os.system("brew install bcftools")
+print("\033[1;45m BCFtools installed \033[0;0;0m")
+print('\n')
+
+print('\033[1;45m All tools have been installed. \033[0;0;0m')
+print('\033[1;45m Use the command "vdb-config -i" to configure your SRA Toolkit \033[0;0;0m')
