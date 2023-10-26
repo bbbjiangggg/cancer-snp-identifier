c36bb7be8dc3a30cb1735f7d6ba58c36c7c7f7cd
Robert Aguilar <robeagui@gmail.com>
2023-10-26 12:51:14 -0400

    Update unt_trimmed_bin_analysis_v1.2.py

--- snp_analysis/unt_trimmed_bin_analysis_v1.2.py
+++ snp_analysis/unt_trimmed_bin_analysis_v1.2.py
@@ -1,13 +1,6 @@
 import os
 import subprocess
 
-def run_command(command):
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.returncode != 0:
-        print(f"Error: {result.stderr}")
-        exit(1)
-    return result.stdout
-
 def process_accession(accession, chromosomes_list):
     # Define the base paths for BWA, Bowtie, and Trimmomatic
     bwa_base_path = "/usr/local/bin/bwa/"
@@ -22,22 +15,22 @@ def process_accession(accession, chromosomes_list):
     # Check if the trimmed file already exists
     if not os.path.isfile(trimmed_file):
         print(f"\n\033[1;35mDownloading sequence {accession} from SRA...\033[0m")
-        run_command(["fastq-dump", accession])
+        subprocess.run(["fastq-dump", accession])
 
         if os.path.isdir(accession):
-            run_command(["rm", "-r", accession])
+            subprocess.run(["rm", "-r", accession])
 
         os.makedirs(accession, exist_ok=True)
-        run_command(["mv", f"{accession}.fastq", accession])
+        subprocess.run(["mv", f"{accession}.fastq", accession])
 
         print(f"\n\033[1;35mRunning fastqc on {accession}...\033[0m")
-        run_command(["fastqc", fastq_file])
+        subprocess.run(["fastqc", fastq_file])
 
         print(f"\n\033[1;35mTrimming {accession}...\033[0m")
-        run_command(["java", "-jar", trimmomatic_path, "SE", "-phred33", fastq_file, trimmed_file, "ILLUMINACLIP:" + truseq3_path + ":2:30:10", "SLIDINGWINDOW:4:20", "MINLEN:35"])
+        subprocess.run(["java", "-jar", trimmomatic_path, "SE", "-phred33", fastq_file, trimmed_file, "ILLUMINACLIP:" + truseq3_path + ":2:30:10", "SLIDINGWINDOW:4:20", "MINLEN:35"])
 
         print(f"\n\033[1;35mRunning fastqc on trimmed {accession}...\033[0m")
-        run_command(["fastqc", trimmed_file])
+        subprocess.run(["fastqc", trimmed_file])
     else:
         print(f"\n\033[1;32mTrimmed file for {accession} already exists. Skipping download, trimming, and quality check...\033[0m")
 
@@ -53,34 +46,23 @@ def process_accession(accession, chromosomes_list):
         print("Bowtie Index Path:", bowtie_index_path)
 
         print(f"\n\033[1;35mMapping {accession} reads using Bowtie2 for chromosome {chromosome}...\033[0m")
-        run_command(["bowtie2", "--very-fast-local", "-x", bowtie_index_path, "-U", trimmed_file, "-S", f"{accession}/{accession}_{chromosome}_mapped.sam"])
+        subprocess.run(["bowtie2", "--very-fast-local", "-x", bowtie_index_path, "-U", trimmed_file, "-S", f"{accession}/{accession}_{chromosome}_mapped.sam"])
 
-        run_command(["samtools", "view", "-S", "-b", f"{accession}/{accession}_{chromosome}_mapped.sam", "-o", f"{accession}/{accession}_{chromosome}_mapped.bam"])
-        run_command(["samtools", "sort", f"{accession}/{accession}_{chromosome}_mapped.bam", "-o", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam"])
+        subprocess.run(["samtools", "view", "-S", "-b", f"{accession}/{accession}_{chromosome}_mapped.sam", "-o", f"{accession}/{accession}_{chromosome}_mapped.bam"])
+        subprocess.run(["samtools", "sort", f"{accession}/{accession}_{chromosome}_mapped.bam", "-o", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam"])
 
-        # Check BAM File Integrity
-        print(f"\n\033[1;35mChecking integrity of BAM file for chromosome {chromosome}...\033[0m")
-        run_command(["samtools", "quickcheck", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam"])
-
-        # Validate BAM File Index
-        print(f"\n\033[1;35mValidating BAM file index for chromosome {chromosome}...\033[0m")
-        bam_index_file = f"{accession}/{accession}_{chromosome}_mapped.sorted.bam.bai"
-        if not os.path.isfile(bam_index_file):
-            print(f"Index file {bam_index_file} does not exist. Creating index...")
-            run_command(["samtools", "index", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam"])
-        else:
-            print(f"Index file {bam_index_file} exists. Checking index...")
-            run_command(["samtools", "quickcheck", bam_index_file])
+        print(f"\n\033[1;35mIndexing BAM file for chromosome {chromosome}...\033[0m")
+        subprocess.run(["samtools", "index", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam"])
 
         print(f"\n\033[1;35mSummarizing the base calls (mpileup) for chromosome {chromosome}...\033[0m")
-        run_command(["bcftools", "mpileup", "-f", bwa_chrom_path, f"{accession}/{accession}_{chromosome}_mapped.sorted.bam", "-o", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf"])
-        run_command(["bcftools", "call", "-mv", "-Ob", "-o", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf"])
+        subprocess.run(["bcftools", "mpileup", "-f", bwa_chrom_path, f"{accession}/{accession}_{chromosome}_mapped.sorted.bam", "-o", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf"])
+        subprocess.run(["bcftools", "call", "-mv", "-Ob", "-o", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf"])
 
         print(f"\n\033[1;35mFinalizing VCF for chromosome {chromosome}...\033[0m")
-        run_command(["bcftools", "view", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf", "-o", f"{accession}/{accession}_{chromosome}_mapped.var.-final.vcf"])
+        subprocess.run(["bcftools", "view", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf", "-o", f"{accession}/{accession}_{chromosome}_mapped.var.-final.vcf"])
 
         # Remove intermediate files for this chromosome
-        run_command(["rm", "-f", f"{accession}/{accession}_{chromosome}_mapped.sam", f"{accession}/{accession}_{chromosome}_mapped.bam", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf"])
+        subprocess.run(["rm", "-f", f"{accession}/{accession}_{chromosome}_mapped.sam", f"{accession}/{accession}_{chromosome}_mapped.bam", f"{accession}/{accession}_{chromosome}_mapped.sorted.bam", f"{accession}/{accession}_{chromosome}_mapped.raw.bcf"])
 
 if __name__ == "__main__":
     # Prompt the user to enter the chromosomes
@@ -97,4 +79,15 @@ if __name__ == "__main__":
             accessions_list = [line.strip() for line in f if line.strip()]
     except Exception as e:
         print(f"An error occurred while reading the file: {e}")
-        exit
+        exit(1)
+
+    # Prompt the user for the number of accession numbers to analyze
+    num_accessions = int(input(f"\nHow many accession numbers from the list do you want to analyze (max {len(accessions_list)}): "))
+    if num_accessions > len(accessions_list):
+        print("The specified number is greater than the number of accession numbers in the list. Exiting.")
+        exit(1)
+
+    # Process the specified number of accession numbers
+    for accession in accessions_list[:num_accessions]:
+        print(f"\nProcessing {accession}...")
+        process_accession(accession, chromosomes_list)
