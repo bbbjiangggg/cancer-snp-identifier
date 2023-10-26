b01fdffb1eda494d2d99005539a7b350568e4ca9
Robert Aguilar <robeagui@gmail.com>
2023-10-25 22:34:24 -0400

    Update snp_analysis.py

--- snp_analysis/snp_analysis.py
+++ snp_analysis/snp_analysis.py
@@ -9,14 +9,19 @@ MAGENTA = '\033[1;35m'
 BLUE = '\033[1;34m'
 RESET = '\033[0m'
 
-def run_analysis():
+def run_analysis(sra):
     cwd = Path.cwd()
-    print(f'{MAGENTA}Running SNP Analysis...{RESET}')
+    print(f'{MAGENTA}Running SNP Analysis for {sra}...{RESET}')
 
     # Add your analysis logic here
 
-    print(f'{GREEN}Analysis Completed!{RESET}')
+    print(f'{GREEN}Analysis for {sra} Completed!{RESET}')
 
 if __name__ == "__main__":
-    run_analysis()
-
+    # Example usage with a command-line argument
+    import sys
+    if len(sys.argv) > 1:
+        sra = sys.argv[1]
+        run_analysis(sra)
+    else:
+        print(f'{RED}Error: Please provide an SRA argument.{RESET}')
