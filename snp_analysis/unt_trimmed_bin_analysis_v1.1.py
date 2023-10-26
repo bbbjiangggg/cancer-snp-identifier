0331d360446332f4d84f3831a8665947024c5308
Robert Aguilar <robeagui@gmail.com>
2023-10-26 07:12:54 -0400

    Update unt_trimmed_bin_analysis_v1.1.py

--- snp_analysis/unt_trimmed_bin_analysis_v1.1.py
+++ snp_analysis/unt_trimmed_bin_analysis_v1.1.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-import shutil
 import subprocess
 from pathlib import Path
 import signal
-import importlib
-
-packages = ['os', 'shutil', 'subprocess', 'pathlib', 'signal']
-
-# Check if packages are installed, install them if necessary
-for package in packages:
-    try:
-        importlib.import_module(package)
-    except ImportError:
-        print(f'{package} is not installed. Installing...')
-        subprocess.run(['pip', 'install', package])
 
 # Define color codes
 RED = '\033[1;31m'
@@ -33,15 +21,72 @@ def replace_file_on_interrupt(sig, frame):
 # Register the signal handler
 signal.signal(signal.SIGINT, replace_file_on_interrupt)
 
-# THIS PROGRAM IS FOR UNTRIMMED WHOLE ANALYSIS
+# Define functions to replace text in files
+def replace_text(file_path, old_text, new_text):
+    with open(file_path, 'r+') as file:
+        text = file.read().replace(old_text, new_text)
+        file.seek(0)
+        file.write(text)
+        file.truncate()
 
-bash_script = f"""#!/bin/bash
+def replace_in_untrimmed_bash_srr(old_text, new_text):
+    replace_text('untrimmed_bash_sra_v1.2.txt', old_text, new_text)
 
+def get_absolute_path(prompt, default_path, variable_name):
+    path = os.path.expanduser(default_path)
+    if os.path.exists(path):
+        print(f'{MAGENTA}{variable_name}){RESET} {path} is the absolute path.')
+    else:
+        print(f'NOTE: {path} does not match your absolute path.')
+        print(f'You have a different path for {variable_name}')
+        path = input(f'{MAGENTA}{variable_name}){RESET} Copy and paste the absolute path to your {variable_name} file: ')
+    replace_in_untrimmed_bash_srr(variable_name, path)
+    return path
+
+def run_analysis(chromosome):
+    # Update the paths in the bash script based on the chromosome
+    bwa_chrom_path = f"/usr/local/bin/bwa/{chromosome}_bwa_ind/Homo_sapiens.GRCh38.dna.chromosome.{chromosome}.fa"
+    bowtie_index_path = f"/usr/local/bin/bowtie/{chromosome}_bowtie_ind/bowtie"
+    replace_in_untrimmed_bash_srr('bowtie_index_path', bowtie_index_path)
+    replace_in_untrimmed_bash_srr('bwa_chrom_path', bwa_chrom_path)
+    
+    # Run the bash script
+    subprocess.run(['bash', str(cwd) + '/untrimmed_bash_sra_v1.2.txt'])
+
+    # Reset the paths in the bash script
+    replace_in_untrimmed_bash_srr(bowtie_index_path, 'bowtie_index_path')
+    replace_in_untrimmed_bash_srr(bwa_chrom_path, 'bwa_chrom_path')
+
+# Get the current working directory
+cwd = Path.cwd()
+
+# Define the paths
+trim_path = get_absolute_path("Enter the absolute path to your trimmomatic-0.39.jar file:", '/usr/local/bin/Trimmomatic-0.39/trimmomatic-0.39.jar', 'trim_path')
+truseq3_path = get_absolute_path("Enter the absolute path to your TruSeq3-SE.fa file:", '/usr/local/bin/Trimmomatic-0.39/adapters/TruSeq3-SE.fa', 'truseq3_path')
+
+# Add the email to be notified when the process is done
+user = input(f'{MAGENTA}1){RESET} Enter the email address to be notified once the analysis is complete: ')
+
+# Add the job title
+job_title = input(f'{MAGENTA}2){RESET} Enter a job name: ')
+
+# This asks the user to type in the path to the accession list
+accession = input(f'{MAGENTA}8){RESET} Copy and paste the name of the accession list file: ')
+
+# Read the accession list file
+with open(accession, 'r') as file:
+    srr_list = [line.strip() for line in file if line.strip()]
+
+# Get the list of chromosomes to analyze
+chromosomes = input(f"{MAGENTA}Enter the chromosome numbers (e.g., 1, 2, ... 22, X, Y) separated by commas to be analyzed: {RESET}").split(',')
+chromosomes = [chrom.strip() for chrom in chromosomes]
+
+# Create a new bash script for each chromosome
+for chromosome in chromosomes:
+    with open("untrimmed_bash_sra_v1.2.txt", "w") as f:
+        bash_script = f"""#!/bin/bash
 # Define the path of the potential trimmed file
 TRIMMED_FILE="SRR_one/SRR_one_trimmed.fq.gz"
-
-# Define the chromosomes to skip
-SKIPPED_CHROMOSOMES="skip_chromosomes"
 
 # Check if the trimmed file already exists
 if [ ! -f "$TRIMMED_FILE" ]; then
@@ -75,112 +120,25 @@ samtools view -S -b SRR_one/SRR_one_mapped.sam > SRR_one/SRR_one_mapped.bam
 echo -e "\n\033[1;35mSorting using Samtools...\033[0m "
 samtools sort SRR_one/SRR_one_mapped.bam > SRR_one/SRR_one_mapped.sorted.bam
 
-# Check if the current chromosome should be skipped
-if [[ ! $SKIPPED_CHROMOSOMES =~ $CURRENT_CHROMOSOME ]]; then
-    echo -e "\n\033[1;35mSummarizing the base calls (mpileup)...\033[0m "
-    bcftools mpileup -f bwa_chrom_path SRR_one/SRR_one_mapped.sorted.bam | bcftools call -mv -Ob -o SRR_one/SRR_one_mapped.raw.bcf
+echo -e "\n\033[1;35mSummarizing the base calls (mpileup)...\033[0m "
+bcftools mpileup -f bwa_chrom_path SRR_one/SRR_one_mapped.sorted.bam | bcftools call -mv -Ob -o SRR_one/SRR_one_mapped.raw.bcf
 
-    echo -e "\n\033[1;35mFinalizing VCF...\033[0m "
-    bcftools view SRR_one/SRR_one_mapped.raw.bcf | vcfutils.pl varFilter - > SRR_one/SRR_one_mapped.var.-final.vcf
-fi
+echo -e "\n\033[1;35mFinalizing VCF...\033[0m "
+bcftools view SRR_one/SRR_one_mapped.raw.bcf | vcfutils.pl varFilter - > SRR_one/SRR_one_mapped.var.-final.vcf
 
-rm SRR_one/SRR_one.fastq SRR_one/SRR_one_mapped.sam SRR_one/SRR_one_mapped.bam SRR_one/SRR_one_mapped.sorted.bam SRR_one/SRR_one_mapped.raw.bcf SRR_one/SRR_one_fastqc.zip SRR_one/SRR_one_trimmed_fastqc.zip
-
+rm SRR_one/SRR_one.fastq SRR_one/SRR_one_mapped.raw.bcf SRR_one/SRR_one_fastqc.zip SRR_one/SRR_one_trimmed_fastqc.zip
 """
+        f.write(bash_script)
+    
+    # Run the analysis for the current chromosome
+    run_analysis(chromosome)
 
-with open("untrimmed_bash_sra_v1.2.txt", "w") as f:
-    f.write(bash_script)
+# Reset the paths
+replace_in_untrimmed_bash_srr(trim_path, 'trim_path')
+replace_in_untrimmed_bash_srr(truseq3_path, 'truseq3_path')
 
-# Define functions to replace text in files
-def replace_text(file_path, old_text, new_text):
-    with open(file_path, 'r+') as file:
-        text = file.read().replace(old_text, new_text)
-        file.seek(0)
-        file.write(text)
-        file.truncate()
+# Send email notification
+print('Sending email to ' + user + ' ....')
+os.system('sendemail -f sudoroot1775@outlook.com -t ' + user + ' -u ' + job_title + '_Analysis Done -m "Ready to receive information for the next analysis." -s smtp-mail.outlook.com:587 -o tls=yes -xu sudoroot1775@outlook.com -xp ydAEwVVu2s7uENC')
 
-def replace_in_untrimmed_bash_srr(old_text, new_text):
-    replace_text('untrimmed_bash_sra_v1.2.txt', old_text, new_text)
-
-# Get the current working directory
-cwd = Path.cwd()
-print('\n')
-
-# Add the email to be notified when the process is done
-user = input(f'{MAGENTA}1){RESET} Enter the email address to be notified once the analysis is complete: ')
-
-# Add the job title
-job_title = input(f'{MAGENTA}2){RESET} Enter a job name: ')
-
-jar_file = '/usr/local/bin/Trimmomatic-0.39/trimmomatic-0.39.jar'
-jar_path = os.path.expanduser(jar_file)
-
-if os.path.exists(jar_path):
-    trim_path = jar_path
-    replace_in_untrimmed_bash_srr('trim_path', trim_path)
-    print(f'{MAGENTA}3){RESET} {jar_path} is the absolute path.')
-else:
-    print(f'NOTE: {jar_path} does not match your absolute path.')
-    print('You have a different path for trimmomatic-0.39.jar')
-    trim_path = input(f'{MAGENTA}3){RESET} Copy and paste the absolute path to your trimmomatic-0.39.jar file: ')
-    replace_in_untrimmed_bash_srr('trim_path', trim_path)
-
-seq3_file = '/usr/local/bin/Trimmomatic-0.39/adapters/TruSeq3-SE.fa'
-seq3_path = os.path.expanduser(seq3_file)
-
-if os.path.exists(seq3_path):
-    truseq3_path = seq3_path
-    replace_in_untrimmed_bash_srr('truseq3_path', truseq3_path)
-    print(f'{MAGENTA}4){RESET} {seq3_path} is the absolute path.')
-else:
-    print(f'NOTE: {seq3_path} does not match your absolute path.')
-    print('You have a different path for TruSeq3-SE.fa')
-    truseq3_path = input(f'{MAGENTA}4){RESET} Copy and paste the absolute path to your TruSeq3-SE.fa file: ')
-    replace_in_untrimmed_bash_srr('truseq3_path', truseq3_path)
-
-index_file = '/usr/local/bin/bowtie2-2.3.4.1-linux-x86_64/indexes/hg19'
-index_path = os.path.expanduser(index_file)
-
-if os.path.exists(index_path):
-    bowtie_index_path = index_path
-    replace_in_untrimmed_bash_srr('bowtie_index_path', bowtie_index_path)
-    print(f'{MAGENTA}5){RESET} {index_path} is the absolute path.')
-else:
-    print(f'NOTE: {index_path} does not match your absolute path.')
-    print('You have a different path for hg19')
-    bowtie_index_path = input(f'{MAGENTA}5){RESET} Copy and paste the absolute path to your hg19 file: ')
-    replace_in_untrimmed_bash_srr('bowtie_index_path', bowtie_index_path)
-
-chrom_file = '/usr/local/bin/bwa.kit/resource-GRCh37/human_g1k_v37.fasta'
-chrom_path = os.path.expanduser(chrom_file)
-
-if os.path.exists(chrom_path):
-    bwa_chrom_path = chrom_path
-    replace_in_untrimmed_bash_srr('bwa_chrom_path', bwa_chrom_path)
-    print(f'{MAGENTA}6){RESET} {chrom_path} is the absolute path.')
-else:
-    print(f'NOTE: {chrom_path} does not match your absolute path.')
-    print('You have a different path for human_g1k_v37.fasta')
-    bwa_chrom_path = input(f'{MAGENTA}6){RESET} Copy and paste the absolute path to your human_g1k_v37.fasta file: ')
-    replace_in_untrimmed_bash_srr('bwa_chrom_path', bwa_chrom_path)
-
-skip_chromosomes = input(f'{MAGENTA}7){RESET} Enter the chromosomes you would like to skip (separated by spaces): ')
-replace_in_untrimmed_bash_srr('skip_chromosomes', skip_chromosomes)
-
-# Save the paths and email to a file
-with open('config.txt', 'w') as config:
-    config.write(f'Email: {user}\n')
-    config.write(f'Job Title: {job_title}\n')
-    config.write(f'Trimmomatic path: {trim_path}\n')
-    config.write(f'TruSeq3 path: {truseq3_path}\n')
-    config.write(f'Bowtie2 index path: {bowtie_index_path}\n')
-    config.write(f'BWA chromosome path: {bwa_chrom_path}\n')
-    config.write(f'Skipped Chromosomes: {skip_chromosomes}\n')
-
-# Make the bash script executable
-os.chmod('untrimmed_bash_sra_v1.2.txt', 0o755)
-
-# Rename the bash script
-shutil.move('untrimmed_bash_sra_v1.2.txt', 'untrimmed_bash_sra_v1.2.sh')
-
-print(f'{GREEN}\nConfiguration complete! You can now run the bash script "untrimmed_bash_sra_v1.2.sh".{RESET}')
+os.remove('untrimmed_bash_sra_v1.2.txt')
