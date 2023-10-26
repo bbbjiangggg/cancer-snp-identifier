6a73f9a8de67920d7dc1842f63e0047deea4908f
Robert Aguilar <robeagui@gmail.com>
2023-10-26 10:50:57 -0400

    Update unt_trimmed_bin_analysis_v1.2.py

--- snp_analysis/unt_trimmed_bin_analysis_v1.2.py
+++ snp_analysis/unt_trimmed_bin_analysis_v1.2.py
@@ -1,5 +1,5 @@
 # Prompt the user to enter the chromosomes
-chromosomes_input = input("Enter the chromosome numbers separated by comma (e.g., 1,2,X,Y) to be analyzed: ")
+chromosomes_input = input("Please enter the chromosomes to be analyzed, separated by a comma: ")
 
 # Split the input string by comma
 chromosomes_list = chromosomes_input.split(',')
@@ -9,3 +9,18 @@ chromosomes_list = [chromosome.strip() for chromosome in chromosomes_list]
 
 # Print the list of chromosomes
 print("List of chromosomes to be analyzed:", chromosomes_list)
+
+# Define the base paths for BWA and Bowtie
+bwa_base_path = "/usr/local/bin/bwa/"
+bowtie_base_path = "/usr/local/bin/bowtie/"
+
+# Print the paths for each chromosome
+for chromosome in chromosomes_list:
+    # Construct the paths
+    bwa_chrom_path = f"{bwa_base_path}{chromosome}_bwa_ind/Homo_sapiens.GRCh38.dna.chromosome.{chromosome}.fa"
+    bowtie_index_path = f"{bowtie_base_path}{chromosome}_bowtie_ind/bowtie"
+
+    # Print the paths
+    print(f"\nPaths for chromosome {chromosome}:")
+    print("BWA Chromosome Path:", bwa_chrom_path)
+    print("Bowtie Index Path:", bowtie_index_path)
