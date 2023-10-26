c0c872d202e10f74ee5f42c2737d4ae3cc3b5023
Robert Aguilar <robeagui@gmail.com>
2023-10-25 22:36:14 -0400

    Update snp_analysis.py

--- snp_analysis/snp_analysis.py
+++ snp_analysis/snp_analysis.py
@@ -9,19 +9,27 @@ MAGENTA = '\033[1;35m'
 BLUE = '\033[1;34m'
 RESET = '\033[0m'
 
-def run_analysis(sra):
+def run_analysis(sra, trim_path, truseq3_path, bowtie_index_path, bwa_chrom_path):
     cwd = Path.cwd()
     print(f'{MAGENTA}Running SNP Analysis for {sra}...{RESET}')
+    print(f'Trim Path: {trim_path}')
+    print(f'TruSeq3 Path: {truseq3_path}')
+    print(f'Bowtie Index Path: {bowtie_index_path}')
+    print(f'BWA Chromosome Path: {bwa_chrom_path}')
 
     # Add your analysis logic here
 
     print(f'{GREEN}Analysis for {sra} Completed!{RESET}')
 
 if __name__ == "__main__":
-    # Example usage with a command-line argument
+    # Example usage with command-line arguments
     import sys
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 5:
         sra = sys.argv[1]
-        run_analysis(sra)
+        trim_path = sys.argv[2]
+        truseq3_path = sys.argv[3]
+        bowtie_index_path = sys.argv[4]
+        bwa_chrom_path = sys.argv[5]
+        run_analysis(sra, trim_path, truseq3_path, bowtie_index_path, bwa_chrom_path)
     else:
-        print(f'{RED}Error: Please provide an SRA argument.{RESET}')
+        print(f'{RED}Error: Please provide the required arguments.{RESET}')
