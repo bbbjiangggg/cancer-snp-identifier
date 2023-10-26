c7c268ac0f4e10199410fd51235e2e7e96d65cef
Robert Aguilar <robeagui@gmail.com>
2023-10-26 15:42:05 -0400

    Update unt_trimmed_bin_analysis_v1.5.py

--- snp_analysis/unt_trimmed_bin_analysis_v1.5.py
+++ snp_analysis/unt_trimmed_bin_analysis_v1.5.py
@@ -55,7 +55,10 @@ def main():
     trimmomatic_path = "/usr/local/bin/Trimmomatic-0.39/trimmomatic-0.39.jar"
     truseq3_path = "/usr/local/bin/Trimmomatic-0.39/adapters/TruSeq3-SE.fa"
 
+    user_email = input("Please enter your email address to receive a notification once the analysis is complete: ").strip()
+    job_title = input("Please enter a job title for this analysis: ").strip()
     accession_list_file = input("Please enter the path to the accession list file: ").strip()
+    
     accession_numbers = read_accession_numbers(accession_list_file)
     
     print(f"\nTotal accession numbers found: {len(accession_numbers)}")
@@ -73,65 +76,60 @@ def main():
     print_chromosome_paths(chromosomes_list, bwa_base_path, bowtie_base_path)
 
     for accession_number in accession_numbers_to_analyze:
-        try:
-            trimmed_file = f"{accession_number}/{accession_number}_trimmed.fq.gz"
-            if not os.path.isfile(trimmed_file):
-                print(f"\n\033[1;35mDownloading number sequence {accession_number} from SRA...\033[0m ")
-                run_command(f"fastq-dump {accession_number}")
+        trimmed_file = f"{accession_number}/{accession_number}_trimmed.fq.gz"
+        if not os.path.isfile(trimmed_file):
+            print(f"\n\033[1;35mDownloading number sequence {accession_number} from SRA...\033[0m ")
+            run_command(f"fastq-dump {accession_number}")
 
-                if os.path.isdir(accession_number):
-                    os.rmdir(accession_number)
-                os.makedirs(accession_number, exist_ok=True)
-                os.rename(f"{accession_number}.fastq", f"{accession_number}/{accession_number}.fastq")
+            if os.path.isdir(accession_number):
+                os.rmdir(accession_number)
+            os.makedirs(accession_number, exist_ok=True)
+            os.rename(f"{accession_number}.fastq", f"{accession_number}/{accession_number}.fastq")
 
-                print(f"\n\033[1;35mRunning fastqc on {accession_number}...\033[0m ")
-                run_command(f"fastqc {accession_number}/{accession_number}.fastq")
+            print(f"\n\033[1;35mRunning fastqc on {accession_number}...\033[0m ")
+            run_command(f"fastqc {accession_number}/{accession_number}.fastq")
 
-                print(f"\n\033[1;35mTrimming {accession_number}...\033[0m ")
-                trim_command = f"java -jar {trimmomatic_path} SE -phred33 {accession_number}/{accession_number}.fastq {trimmed_file} ILLUMINACLIP:{truseq3_path}:2:30:10 SLIDINGWINDOW:4:20 MINLEN:35"
-                run_command(trim_command)
+            print(f"\n\033[1;35mTrimming {accession_number}...\033[0m ")
+            trim_command = f"java -jar {trimmomatic_path} SE -phred33 {accession_number}/{accession_number}.fastq {trimmed_file} ILLUMINACLIP:{truseq3_path}:2:30:10 SLIDINGWINDOW:4:20 MINLEN:35"
+            run_command(trim_command)
 
-                print(f"\n\033[1;35mRunning fastqc on trimmed {accession_number}...\033[0m ")
-                run_command(f"fastqc {trimmed_file}")
-            else:
-                print("\n\033[1;32mTrimmed file already exists. Skipping download, trimming, and quality check...\033[0m")
+            print(f"\n\033[1;35mRunning fastqc on trimmed {accession_number}...\033[0m ")
+            run_command(f"fastqc {trimmed_file}")
+        else:
+            print("\n\033[1;32mTrimmed file already exists. Skipping download, trimming, and quality check...\033[0m")
 
-            for chromosome in chromosomes_list:
-                final_vcf_file = f"{accession_number}/{accession_number}_mapped_{chromosome}.var.-final.vcf"
-                if os.path.isfile(final_vcf_file) and not is_file_empty(final_vcf_file):
-                    print(f"\n\033[1;32mVCF file for {accession_number}, chromosome {chromosome} already exists. Skipping analysis...\033[0m")
-                    continue
-                elif is_file_empty(final_vcf_file):
-                    print(f"\n\033[1;33mVCF file for {accession_number}, chromosome {chromosome} is empty. Deleting and adding to analysis...\033[0m")
-                    os.remove(final_vcf_file)
+        for chromosome in chromosomes_list:
+            final_vcf_file = f"{accession_number}/{accession_number}_mapped_{chromosome}.var.-final.vcf"
+            if os.path.isfile(final_vcf_file) and not is_file_empty(final_vcf_file):
+                print(f"\n\033[1;32mVCF file for {accession_number}, chromosome {chromosome} already exists. Skipping analysis...\033[0m")
+                continue
+            elif is_file_empty(final_vcf_file):
+                print(f"\n\033[1;33mVCF file for {accession_number}, chromosome {chromosome} is empty. Deleting and adding to analysis...\033[0m")
+                os.remove(final_vcf_file)
 
-                bwa_chrom_path = f"{bwa_base_path}{chromosome}_bwa_ind/Homo_sapiens.GRCh38.dna.chromosome.{chromosome}.fa"
-                bowtie_index_path = f"{bowtie_base_path}{chromosome}_bowtie_ind/bowtie"
+            bwa_chrom_path = f"{bwa_base_path}{chromosome}_bwa_ind/Homo_sapiens.GRCh38.dna.chromosome.{chromosome}.fa"
+            bowtie_index_path = f"{bowtie_base_path}{chromosome}_bowtie_ind/bowtie"
 
-                print(f"\n\033[1;35mMapping {accession_number} reads using Bowtie2 for chromosome {chromosome}...\033[0m ")
-                run_command(f"bowtie2 --very-fast-local -x {bowtie_index_path} {trimmed_file} -S {accession_number}/{accession_number}_mapped_{chromosome}.sam")
+            print(f"\n\033[1;35mMapping {accession_number} reads using Bowtie2 for chromosome {chromosome}...\033[0m ")
+            run_command(f"bowtie2 --very-fast-local -x {bowtie_index_path} {trimmed_file} -S {accession_number}/{accession_number}_mapped_{chromosome}.sam")
 
-                run_command(f"samtools view -S -b {accession_number}/{accession_number}_mapped_{chromosome}.sam > {accession_number}/{accession_number}_mapped_{chromosome}.bam")
+            run_command(f"samtools view -S -b {accession_number}/{accession_number}_mapped_{chromosome}.sam > {accession_number}/{accession_number}_mapped_{chromosome}.bam")
 
-                print("\n\033[1;35mSorting using Samtools...\033[0m ")
-                run_command(f"samtools sort {accession_number}/{accession_number}_mapped_{chromosome}.bam > {accession_number}/{accession_number}_mapped_{chromosome}.sorted.bam")
+            print("\n\033[1;35mSorting using Samtools...\033[0m ")
+            run_command(f"samtools sort {accession_number}/{accession_number}_mapped_{chromosome}.bam > {accession_number}/{accession_number}_mapped_{chromosome}.sorted.bam")
 
-                print("\n\033[1;35mSummarizing the base calls (mpileup)...\033[0m ")
-                run_command(f"bcftools mpileup -f {bwa_chrom_path} {accession_number}/{accession_number}_mapped_{chromosome}.sorted.bam | bcftools call -mv -Ob -o {accession_number}/{accession_number}_mapped_{chromosome}.raw.bcf")
+            print("\n\033[1;35mSummarizing the base calls (mpileup)...\033[0m ")
+            run_command(f"bcftools mpileup -f {bwa_chrom_path} {accession_number}/{accession_number}_mapped_{chromosome}.sorted.bam | bcftools call -mv -Ob -o {accession_number}/{accession_number}_mapped_{chromosome}.raw.bcf")
 
-                print("\n\033[1;35mFinalizing VCF...\033[0m ")
-                run_command(f"bcftools view {accession_number}/{accession_number}_mapped_{chromosome}.raw.bcf | vcfutils.pl varFilter - > {final_vcf_file}")
+            print("\n\033[1;35mFinalizing VCF...\033[0m ")
+            run_command(f"bcftools view {accession_number}/{accession_number}_mapped_{chromosome}.raw.bcf | vcfutils.pl varFilter - > {final_vcf_file}")
 
-                # Delete intermediate files to save disk space
-                delete_intermediate_files(accession_number, chromosome)
+            # Delete intermediate files to save disk space
+            delete_intermediate_files(accession_number, chromosome)
 
-        except KeyboardInterrupt:
-            print("\nAnalysis interrupted by user. Exiting.")
-            sys.exit(1)
+    send_email_command = f'sendemail -f sudoroot1775@outlook.com -t {user_email} -u "{job_title}_Analysis Done" -m "Ready to receive information for the next analysis." -s smtp-mail.outlook.com:587 -o tls=yes -xu sudoroot1775@outlook.com -xp ydAEwVVu2s7uENC'
+    os.system(send_email_command)
+    print("Analysis complete! A notification email has been sent.")
 
 if __name__ == "__main__":
     main()
-    user_email = input("Please enter your email address to receive a notification once the analysis is complete: ").strip()
-    job_title = input("Please enter a job title for this analysis: ").strip()
-    send_email_command = f'sendemail -f sudoroot1775@outlook.com -t {user_email} -u "{job_title}_Analysis Done" -m "Ready to receive information for the next analysis." -s smtp-mail.outlook.com:587 -o tls=yes -xu sudoroot1775@outlook.com -xp ydAEwVVu2s7uENC'
-    os.system(send_email_command)
