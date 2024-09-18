22ac07c7fa8c23935ef430afb321c6a6eda38ce7
Robert Aguilar <robeagui@gmail.com>
2024-09-17 23:40:51 -0400

    new pipe

---
+++ snp_analysis/snp_analysis_pipeline_v2/command_execution.py
@@ -0,0 +1,14 @@
+# command_execution.py
+import subprocess
+import sys
+from snp_analysis_pipeline_v2.logging_module import log_message
+
+def run_command(command):
+    try:
+        subprocess.run(command, check=True, shell=True)
+    except subprocess.CalledProcessError as e:
+        log_message(f"An error occurred: {e}", level="error")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        log_message("Analysis interrupted by user. Exiting.", level="error")
+        sys.exit(1)
