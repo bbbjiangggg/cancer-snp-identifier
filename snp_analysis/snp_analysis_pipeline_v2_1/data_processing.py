abf60eb73bb8b039eb6e060f4d418e594290a1eb
Robert Aguilar <robeagui@gmail.com>
2024-11-07 19:56:25 -0500

    Update data_processing.py

--- snp_analysis/snp_analysis_pipeline_v2_1/data_processing.py
+++ snp_analysis/snp_analysis_pipeline_v2_1/data_processing.py
@@ -27,18 +27,29 @@ def detect_read_type(accession_number):
 
 def prefetch_and_convert(accession_number, threads):
     """Run prefetch and fasterq-dump to download and convert SRA files."""
+    # Ask user if they want to use fastq-dump instead of prefetch and fasterq-dump
+    use_fastq_dump = input("Would you like to use fastq-dump instead of prefetch and fasterq-dump? (yes/y or no/n): ").strip().lower()
+    
     # Skip downloading if trimmed files are already present
     if os.path.isfile(f"{accession_number}/{accession_number}_trimmed.fq") or (
         os.path.isfile(f"{accession_number}/{accession_number}_1_trimmed.fq") and os.path.isfile(f"{accession_number}/{accession_number}_2_trimmed.fq")):
         log_message(f"Trimmed files already exist for {accession_number}. Skipping download and conversion.", level="info")
         return
 
-    log_message(f"\nDownloading sequence number {accession_number} with prefetch...", level="info")
-    run_command(f"prefetch {accession_number}")
+    if use_fastq_dump in ['yes', 'y']:
+        log_message(f"\nDownloading and converting {accession_number} with fastq-dump...", level="info")
+        if detect_read_type(accession_number) == '2':
+            run_command(f"fastq-dump --split-files {accession_number} --outdir {accession_number}")
+        else:
+            run_command(f"fastq-dump {accession_number} --outdir {accession_number}")
+    else:
+        log_message(f"\nDownloading sequence number {accession_number} with prefetch...", level="info")
+        run_command(f"prefetch {accession_number}")
 
-    log_message(f"\nConverting {accession_number} with fasterq-dump...", level="info")
-    run_command(f"fasterq-dump {accession_number} --threads {threads} --progress")
+        log_message(f"\nConverting {accession_number} with fasterq-dump...", level="info")
+        run_command(f"fasterq-dump {accession_number} --threads {threads} --progress")
 
+    # Move fastq files to accession directory if not already there
     if os.path.isfile(f"{accession_number}.fastq"):
         shutil.move(f"{accession_number}.fastq", f"{accession_number}/{accession_number}.fastq")
     if os.path.isfile(f"{accession_number}_1.fastq"):
